89af53fd3ed66ce448725aaf307f16a5f49a3db7
titanke <81938777+titanke@users.noreply.github.com>
2024-03-31 21:50:42 -0500

    cambios static

--- core/settings.py
+++ core/settings.py
@@ -168,7 +168,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #if not DEBUG:
-#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
