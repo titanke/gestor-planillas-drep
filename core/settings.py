c9178bfc36b20c9e9bafd84d2d9c7bbf9c9b93f1
titanke <81938777+titanke@users.noreply.github.com>
2024-06-03 10:12:51 -0500

    cambios2do desp

--- core/settings.py
+++ core/settings.py
@@ -171,7 +171,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "UTC+5"
 
 USE_I18N = True
 
@@ -187,9 +187,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
